3eae42deabe25187a488253e0fb32e28f8209b64
Martin Brugnara <martin.brugnara@gmail.com>
2014-12-04 10:45:05 +0100

    improve lex helper

--- 1.2 - lex home exercises/lexcompile.sh
+++ 1.2 - lex home exercises/lexcompile.sh
@@ -1,3 +1,12 @@
-lex $1.l
-gcc -c -o $1.o lex.yy.c
-gcc $1.o -ll -o $1
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+    echo "Usage: ./lexcompile.sh file_name.l(ex)"
+    exit 1
+fi
+
+FNAME="${1}.out"
+
+lex $1
+gcc -c -o $FNAME lex.yy.c && rm lex.yy.c &&\
+gcc $FNAME -ll -o $FNAME
